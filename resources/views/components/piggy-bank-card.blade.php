bc09819b611344c0c2f1b20305ecc635e2322bf2
Ubeydullah Keleş <u.keles@gmail.com>
2024-11-25 17:54:02 +0300

    update font weight to sustain consistency in style

--- resources/views/components/piggy-bank-card.blade.php
+++ resources/views/components/piggy-bank-card.blade.php
@@ -1,7 +1,7 @@
 <div class="p-4 border rounded-lg shadow">
     <h3 class="text-lg font-bold">{{ $piggyBank->name }}</h3>
-    <p class="text-sm text-gray-600">{{ __('price') }}: {{ $piggyBank->price }}</p>
-    <p class="text-sm text-gray-600">{{ __('starting_amount') }}: {{ $piggyBank->starting_amount }}</p>
-    <p class="text-sm text-gray-600">{{ __('remaining_amount') }}: {{ $piggyBank->price - $piggyBank->starting_amount }}</p>
-    <p class="text-sm text-gray-600">{{ __('purchase_date') }}: {{ $piggyBank->purchase_date }}</p>
+    <p class="text-sm text-gray-600 font-medium">{{ __('price') }}: {{ $piggyBank->price }}</p>
+    <p class="text-sm text-gray-600 font-medium">{{ __('starting_amount') }}: {{ $piggyBank->starting_amount }}</p>
+    <p class="text-sm text-gray-600 font-medium">{{ __('remaining_amount') }}: {{ $piggyBank->price - $piggyBank->starting_amount }}</p>
+    <p class="text-sm text-gray-600 font-medium">{{ __('purchase_date') }}: {{ $piggyBank->purchase_date }}</p>
 </div>
