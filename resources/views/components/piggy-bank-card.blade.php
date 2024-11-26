92e3d326e495a4fa129e843bbc0e84212d161076
Ubeydullah Keleş <u.keles@gmail.com>
2024-11-26 13:10:12 +0300

    give background color and hover effect to piggy bank card component

--- resources/views/components/piggy-bank-card.blade.php
+++ resources/views/components/piggy-bank-card.blade.php
@@ -1,4 +1,4 @@
-<div class="p-4 border rounded-lg shadow">
+<div class="p-4 border rounded-lg shadow bg-rose-50 hover:bg-rose-100 transition-colors duration-300">
     <h3 class="text-lg font-bold">{{ $piggyBank->name }}</h3>
     <p class="text-sm text-gray-600 font-medium">{{ __('price') }}: {{ $piggyBank->price }}</p>
     <p class="text-sm text-gray-600 font-medium">{{ __('starting_amount') }}: {{ $piggyBank->starting_amount }}</p>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,11 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
-                        {{ __('Welcome') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('piggy-banks.index')" :active="request()->routeIs('piggy-banks.index')">
                         {{ __('My Piggy Banks') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
+                        {{ __('Welcome') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -40,6 +40,7 @@
                     </x-slot>
 
                     <x-slot name="content">
+
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
@@ -78,6 +79,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('piggy-banks.index')" :active="request()->routeIs('piggy-banks.index')">
+                {{ __('My Piggy Banks') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
                 {{ __('Welcome') }}
