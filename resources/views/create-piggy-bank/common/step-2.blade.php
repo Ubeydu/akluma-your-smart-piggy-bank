5104399b782555b236b8ecd6d5d320aed18f8d1c
Ubeydullah Keleş <u.keles@gmail.com>
2024-12-10 15:29:22 +0300

    fix card width inconsistency

--- resources/views/create-piggy-bank/common/step-2.blade.php
+++ resources/views/create-piggy-bank/common/step-2.blade.php
@@ -17,7 +17,7 @@
                         <form action="{{ route('create-piggy-bank.choose-strategy') }}" method="POST">
                             @csrf
                             <input type="hidden" name="strategy" value="pick-date">
-                            <button type="submit" class="p-6 text-left border rounded-lg hover:border-indigo-500 focus:outline-none focus:border-indigo-500 transition-colors duration-200">
+                            <button type="submit" class="w-full p-6 text-left border rounded-lg hover:border-indigo-500 focus:outline-none focus:border-indigo-500 transition-colors duration-200">
                                 <h3 class="text-xl font-semibold mb-3">{{ __('Pick Date') }}</h3>
                                 <p class="text-gray-600">{{ __('pick_date_strategy_definition') }}</p>
                             </button>
@@ -27,7 +27,7 @@
                         <form action="{{ route('create-piggy-bank.choose-strategy') }}" method="POST">
                             @csrf
                             <input type="hidden" name="strategy" value="enter-saving-amount">
-                            <button type="submit" class="p-6 text-left border rounded-lg hover:border-indigo-500 focus:outline-none focus:border-indigo-500 transition-colors duration-200">
+                            <button type="submit" class="w-full p-6 text-left border rounded-lg hover:border-indigo-500 focus:outline-none focus:border-indigo-500 transition-colors duration-200">
                                 <h3 class="text-xl font-semibold mb-3">{{ __('Enter Saving Amount') }}</h3>
                                 <p class="text-gray-600">{{ __('enter_saving_amount_strategy_definition') }}</p>
                             </button>
