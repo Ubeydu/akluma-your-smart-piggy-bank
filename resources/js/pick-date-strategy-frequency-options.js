837cc4c5b921b1224d858e3e9dc1e2884a2bc82c
Ubeydullah Keleş <u.keles@gmail.com>
2025-01-30 18:15:51 +0300

    add tooltip for extra amount to 2 pages

--- resources/js/pick-date-strategy-frequency-options.js
+++ resources/js/pick-date-strategy-frequency-options.js
@@ -275,10 +275,42 @@ document.addEventListener("DOMContentLoaded", function () {
             <span data-translate="target-amount">${window.Laravel.translations['Target']}:</span>
             <span>${formatAmount(option.target_amount.formatted_value)}</span>
         </div>
+
+
         <div class="flex justify-between text-green-600">
-            <span data-translate="extra-savings">${window.Laravel.translations['Extra']}:</span>
-            <span>+${formatAmount(option.extra_savings.formatted_value)}</span>
-        </div>
+    <span class="flex items-center gap-1">
+        <span data-translate="extra-savings">${window.Laravel.translations['Extra']}:</span>
+        <span x-data="{ showTooltip: false }" class="relative cursor-help">
+            <svg @mouseenter="showTooltip = true"
+                @mouseleave="showTooltip = false"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                class="w-4 h-4 text-green-600 hover:text-gray-800 transition-colors duration-200">
+                <path stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+            </svg>
+            <div x-show="showTooltip"
+    x-cloak
+    class="absolute z-10 w-48 px-3 py-2 mt-2 text-sm bg-gray-900 text-white rounded-lg shadow-lg"
+    :class="{
+        'left-1/2 -translate-x-1/2': window.innerWidth > 640, /* Center it on larger screens */
+        'left-0 ml-2': window.innerWidth <= 640 && $el.getBoundingClientRect().left < 150, /* Shift right if near left edge */
+        'right-0 mr-2': window.innerWidth <= 640 && $el.getBoundingClientRect().right > window.innerWidth - 150 /* Shift left if near right edge */
+    }"
+    role="tooltip">
+
+                ${window.Laravel.translations['Extra Savings Tooltip Info']}
+            </div>
+        </span>
+    </span>
+    <span>+${formatAmount(option.extra_savings.formatted_value)}</span>
+</div>
+
+
         <div class="flex justify-between font-semibold">
             <span data-translate="total-savings">${window.Laravel.translations['Total']}:</span>
             <span>${formatAmount(option.total_savings.formatted_value)}</span>
