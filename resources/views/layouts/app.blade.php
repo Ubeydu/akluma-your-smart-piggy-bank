024c1685b40575e8208425bf7e1fab368103ebd7
Ubeydullah Keleş <u.keles@gmail.com>
2025-03-05 12:11:47 +0000

    fix header for mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,11 +22,11 @@
             @if(Auth::check() || !isset($useWelcomeLayout))
                 @include('layouts.navigation')
             @else
-                <div class="bg-gray-50 text-black/50">
-                    <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl mx-auto">
+
+                    <div class="w-full">
                         <x-unauthenticated-header />
                     </div>
-                </div>
+
             @endif
 
             <!-- Page Heading -->
